f40fada37f469dac615c374ee2279942ff3cc084
gregroberts <greg.roberts1991@gmail.com>
2015-03-23 14:12:48 +0000

    Written Scraper

--- Scraper/Crawlers/HTTP_GET.py
+++ Scraper/Crawlers/HTTP_GET.py
@@ -1,11 +1,13 @@
 from requests import get as r_get
 from retry import retry
-import requests.exceptions.RequestException as excep
+from requests.exceptions import RequestException as excep
 
-@retry(excep, tries=5, delay=2, backoff=2))
-def get(url, auth = None):
-	if auth is not None:
-		res = requests.get(url, auth=auth)
-	else:
-		res = requests.get(url)
-	return res
+
+class HTTP_GET:
+	@retry(excep, tries=5, delay=2, backoff=2)
+	def _get(self, url, auth = None):
+		if auth is not None:
+			res = r_get(url, auth=auth)
+		else:
+			res = r_get(url)
+		return res
--- Scraper/Scraper.py
+++ Scraper/Scraper.py
@@ -1,65 +1,81 @@
+from Crawlers.htmlxpath import crawler
+from pprint import PrettyPrinter
+from importlib import import_module
 
 
-def recurse(doc, crawler, query):
-	'''This is the guy who hydrates the query.
-	three possibilities:
-	encounters a query object - 
-		finds @base
-		returns recurse(@base,crawler, @query)
-	encounters a plain dict - 
-		returns {i:recurse(j)}
-	encounters a string - 
-		assume is a query, return parse(j)
-	'''
-	if is_qry_obj(query):
-		return handle(doc, crawler, query)
-	elif isinstance(query, dict):
-		return {i:recurse(doc, crawler, j) for i, j in query.items()}
-	elif isinstance(query, basestring):
-		return parse(doc, crawler, query)
+class Query(object):
+	def __init__(self, crawler, query, base = None):
+			self.crawler = crawler
+			if base is not None:
+				self.base = base
+			else:
+				self.base = self.crawler.base
+			self.resp = self.execute(query)
 
+	def ret_sub(self, new_base, new_query):
+		if isinstance(new_base, basestring):
+			new_base = self.crawler.get(new_base)
+		new_obj = Query(self.crawler, new_query, new_base)
+		return new_obj.resp
 
-def handle(doc, crawler, query):
-	'''handles a query object
-		finds whatever @base is,
-		decides what to do at that point.
-		could either return a node
-	'''
-	if doc == '@@START@@':
-		#we're at the top
-		actual_base = crawler.get(query['@base'])
-	else:
-		actual_base = crawler.query(query['@base'])
-	if isinstance(actual_base, basestring):
-		#it's a reference to another doc
-		actual_base = crawler.get(query['@base'])
-	return recurse(actual_base, crawler, query['@query'])
-
-
-def is_qry_obj(obj):
-	'''checks if it's a query object or not'''
-	if isinstance(obj, dict):
-		keys = [str(i) for i in obj.keys()]
-		if keys == ['@base','@freshness']:
-			return True
+	def sub_test(self, obj):
+		if obj.keys() == ['@base','@query']:
+			new_base = self.crawler.query(obj['@base'], self.base)
+			if isinstance(new_base, list):
+				new_obj = [self.ret_sub(i, obj['@query']) for i in new_base]
+			else:
+				new_obj = self.ret_sub(new_base, obj['@query'])
+			return new_obj
 		else:
-			return False
-	else:
-		return False
+			return None
+
+	def execute(self, query):
+		q_dict = lambda x: {i : self.execute(j) for i, j in x.items()}
+		q_list = lambda x: [self.execute(i) for i in x]
+		if isinstance(query,dict):
+			resp = self.sub_test(query) or q_dict(query)
+		elif isinstance(query,list):
+			resp = q_list(query)
+		elif isinstance(query, basestring):
+			resp = self.crawler.query(query, self.base)
+		else:
+			print type(query)
+		return resp
+
+class Scrape:
+	def __init__(self, base, query, crawler_name = 'htmlxpath'):
+		crawler = import_module('Crawlers.%s' %  crawler_name)
+		self.crawler = crawler.crawler(base)
+		self.query = query
+
+	def go(self):
+		query = Query(self.crawler, self.query)
+		return query.resp
 
 
-def parse(doc, crawler, query):
-	'''smallest building block.
-	Takes:
-	 some kind of document (doc)
-	 some kind of crawler (which has a method query)
-	 & a query to execute (query).
-	Returns the result, 
-	or the exception thrown'''
-	try:
-		result = crawler.query(doc, query)
-	except Exception as e:
-		result = e
-	return result
 
 
+if __name__ == '__main__':
+#BASIC TEST
+	query = {
+		"t":'//title//text()',
+		"f":{
+			"@base":"//ul[1]//a/@href",
+			"@query":{
+				"title":"//title//text()",
+				"links":{
+					"@base":"//a",
+					"@query":{
+						"text":".//text()"
+					}
+				}	
+			}
+		}
+	}
+#	lxml = crawler('http://lxml.de')
+#	a = Query(lxml, query)
+	pp = PrettyPrinter()
+#	pp.pprint(a.resp)
+#SCRAPER TEST
+	gg = Scrape('http://lxml.de',query, 'htmlxpath')
+	pp.pprint(gg.go())
