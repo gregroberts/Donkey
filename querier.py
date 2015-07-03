960dc0165b7bd1fd24c6f48c331809f731a6b868
Greg Roberts <greg.roberts1991@gmail.com>
2015-07-03 16:56:59 +0100

    barely did anything

---
+++ querier.py
@@ -0,0 +1,9 @@
+from grabber import request
+from handler import handle
+
+#here we define the different possible flows one may use to fulfill data requests
+
+
+#start with the obvious one that just uses request | handle
+#but it is left to be extensible
+
