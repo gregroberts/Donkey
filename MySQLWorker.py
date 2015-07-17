3280e2b2b2eec71e7cabf5a52e407061e89d73d6
Greg Roberts <greg.roberts1991@gmail.com>
2015-07-17 11:35:40 +0100

    iuniun

--- MySQLWorker.py
+++ MySQLWorker.py
@@ -52,57 +52,57 @@ class MySQLWorker(Worker):
 								passwd=donk_conf.MySQL_passwd,
 								port=donk_conf.MySQL_port)
 
-    def perform_job(self, job):
-        """Performs the actual work of a job.  Will/should only be called
-        inside the work horse's process.
-        """
-        self.prepare_job_execution(job)
-        job.kwargs['db_conn'] = self.mysql_conn
-        with self.connection._pipeline() as pipeline:
-            started_job_registry = StartedJobRegistry(job.origin, self.connection)
+	def perform_job(self, job):
+		"""Performs the actual work of a job.  Will/should only be called
+		inside the work horse's process.
+		"""
+		self.prepare_job_execution(job)
+		job.kwargs['db_conn'] = self.mysql_conn
+		with self.connection._pipeline() as pipeline:
+			started_job_registry = StartedJobRegistry(job.origin, self.connection)
 
-            try:
-                with self.death_penalty_class(job.timeout or self.queue_class.DEFAULT_TIMEOUT):
-                    rv = job.perform()
+			try:
+				with self.death_penalty_class(job.timeout or self.queue_class.DEFAULT_TIMEOUT):
+					rv = job.perform()
 
-                # Pickle the result in the same try-except block since we need
-                # to use the same exc handling when pickling fails
-                job._result = rv
+				# Pickle the result in the same try-except block since we need
+				# to use the same exc handling when pickling fails
+				job._result = rv
 
-                self.set_current_job_id(None, pipeline=pipeline)
+				self.set_current_job_id(None, pipeline=pipeline)
 
-                result_ttl = job.get_result_ttl(self.default_result_ttl)
-                if result_ttl != 0:
-                    job.ended_at = utcnow()
-                    job._status = JobStatus.FINISHED
-                    job.save(pipeline=pipeline)
+				result_ttl = job.get_result_ttl(self.default_result_ttl)
+				if result_ttl != 0:
+					job.ended_at = utcnow()
+					job._status = JobStatus.FINISHED
+					job.save(pipeline=pipeline)
 
-                    finished_job_registry = FinishedJobRegistry(job.origin, self.connection)
-                    finished_job_registry.add(job, result_ttl, pipeline)
+					finished_job_registry = FinishedJobRegistry(job.origin, self.connection)
+					finished_job_registry.add(job, result_ttl, pipeline)
 
-                job.cleanup(result_ttl, pipeline=pipeline)
-                started_job_registry.remove(job, pipeline=pipeline)
+				job.cleanup(result_ttl, pipeline=pipeline)
+				started_job_registry.remove(job, pipeline=pipeline)
 
-                pipeline.execute()
+				pipeline.execute()
 
-            except Exception:
-                job.set_status(JobStatus.FAILED, pipeline=pipeline)
-                started_job_registry.remove(job, pipeline=pipeline)
-                pipeline.execute()
-                self.handle_exception(job, *sys.exc_info())
-                return False
+			except Exception:
+				job.set_status(JobStatus.FAILED, pipeline=pipeline)
+				started_job_registry.remove(job, pipeline=pipeline)
+				pipeline.execute()
+				self.handle_exception(job, *sys.exc_info())
+				return False
 
-        if rv is None:
-            self.log.info('Job OK')
-        else:
-            self.log.info('Job OK, result = {0!r}'.format(yellow(text_type(rv))))
+		if rv is None:
+			self.log.info('Job OK')
+		else:
+			self.log.info('Job OK, result = {0!r}'.format(yellow(text_type(rv))))
 
-        if result_ttl == 0:
-            self.log.info('Result discarded immediately')
-        elif result_ttl > 0:
-            self.log.info('Result is kept for {0} seconds'.format(result_ttl))
-        else:
-            self.log.warning('Result will never expire, clean up result key manually')
+		if result_ttl == 0:
+			self.log.info('Result discarded immediately')
+		elif result_ttl > 0:
+			self.log.info('Result is kept for {0} seconds'.format(result_ttl))
+		else:
+			self.log.warning('Result will never expire, clean up result key manually')
 
-        return True
+		return True
 
