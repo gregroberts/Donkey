22844144b1489f98e17b883767a402f5a8efdf89
Greg Roberts <greg.roberts1991@gmail.com>
2015-07-23 09:19:33 +0100

    WORK THAT LOVELY HORSE

--- workit.py
+++ workit.py
@@ -6,8 +6,8 @@ import sys
 #this guy is intended as a wrapper for MySQLWorker
 #so you can instantiate it from a terminal and specify queue names and an interval
 #Usage:
-#python workit.py 2 collect_low collect_high
+#python workit.py 2 my_lovely_horse collect_low collect_high
 with Connection():
-	q = map(Queue, sys.argv[2:]) or [Queue()]
-	w = MySQLWorker(q, interval = sys.argv[1])
+	q = map(Queue, sys.argv[3:]) or [Queue()]
+	w = MySQLWorker(q, interval = sys.argv[1], name = sys.argv[2])
 	w.work()
